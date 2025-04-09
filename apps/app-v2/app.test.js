914bfdc680eb261596aed333ca6a34847763ede9
EdgarEktibaryan <edgar.ektibaryan@gmail.com>
2025-04-09 15:06:28 +0400

    test js file fix

--- apps/app-v2/app.test.js
+++ apps/app-v2/app.test.js
@@ -2,9 +2,10 @@ const request = require('supertest');
 const app = require('./app');
 
 describe('GET /', () => {
-  it('should return Hello World message', async () => {
+  it('should return HTML with Version 1 in it', async () => {
     const res = await request(app).get('/');
     expect(res.statusCode).toBe(200);
-    expect(res.text).toBe('Version 2');
+    expect(res.text).toContain('Version 1');
+    expect(res.headers['content-type']).toMatch(/html/);
   });
 });
